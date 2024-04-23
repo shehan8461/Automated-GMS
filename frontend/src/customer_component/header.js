350311a303cff7e9de57c5628ff99ade3067348c
Shehan <shehansalitha1999@gmail.com>
2024-04-23 19:16:38 +0530

    third marketing

--- frontend/src/customer_component/header.js
+++ frontend/src/customer_component/header.js
@@ -10,7 +10,7 @@ function Header(){
             <a href="/feedback_details"><li>Customer Feedback Details</li> </a> 
              <a href="/add_feedback"><li>Enter Feedback</li> </a> 
              <a href="/add_discount"><li>Add Discount</li> </a> 
-             <a href="/discountdetails"><li> Discount details</li> </a> 
+             <a href="/discountdetails"><li>Discount details</li> </a> 
             <li>About</li>
             <li>Contacts</li>
             
