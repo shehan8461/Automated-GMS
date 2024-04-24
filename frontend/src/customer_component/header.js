b6aeaef97ca2c5196601a9f222c39587a3624ae2
Shehan <shehansalitha1999@gmail.com>
2024-04-24 12:04:06 +0530

    seven commit marketing manager

--- frontend/src/customer_component/header.js
+++ frontend/src/customer_component/header.js
@@ -5,8 +5,9 @@ function Header(){
  return(
     <div>
         <nav>
-        <ul>
-            <a href="/"><li>Dash Board</li> </a>
+        <ul>    
+            <a href="/"><li>Home</li> </a>
+            <a href="/marketing"><li>Dash Board</li> </a>
             <a href="/feedback_details"><li>Customer Feedback Details</li> </a> 
              <a href="/add_feedback"><li>Enter Feedback</li> </a> 
              <a href="/add_discount"><li>Add Discount</li> </a> 
