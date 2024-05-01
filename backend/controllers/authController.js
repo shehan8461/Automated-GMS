f01a1eae84ac41c6b2fefea26568d5980eeadae6
Himansa2002 <it22219398@sliit.lk>
2024-05-01 18:48:59 +0530

    I changed bcrypt into bcrptjs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const USER_ROLES = require("../constants/roles_OM");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
