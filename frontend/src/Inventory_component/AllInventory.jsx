702b4900d9f341e83892d4889d2fdba07c0d5b84
salini02 <salini.herath8@gmail.com>
2024-05-16 10:12:31 +0530

    inventory editTable

--- frontend/src/Inventory_component/AllInventory.jsx
+++ frontend/src/Inventory_component/AllInventory.jsx
@@ -123,7 +123,7 @@ export default function AllInventory() {
                                 </div>
                             </div>
                             <div className="overflow-x-auto">
-                                <table id="inventory-table" className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <table style={{marginLeft:'0px'}} id="inventory-table" className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                         <tr>
                                             <th scope="col" className="px-4 py-4">Product Name</th>
